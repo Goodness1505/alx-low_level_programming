1315cf7e8660f94244708660639177daf15c7622
Goodness1505 <goodnessmaemile1505@gmail.com>
2023-07-15 10:00:50 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/2-print_aphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Determines if a number is positive.negetive or zero
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char alp[26] = "abcdefghijklmnopqrstuwxyz";
+        int i;
+
+        for (i = 0; i < 26; i++)
+        {
+                putchar(alp[i]);
+        }
+        putcher('\n');
+        return (0);
+}

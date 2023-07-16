3d5d53af64dc69b8a5f9707883d86cdde16f3c08
Goodness1505 <goodnessmaemile1505@gmail.com>
2023-07-15 17:15:25 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**main - prints the alphabetic
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'n' ; c <= 'r';c++)
+	{
+	      putchar(c)
+	}
+	       putchar('\n')';'
+	       return(0)';'
+
+}
+		     

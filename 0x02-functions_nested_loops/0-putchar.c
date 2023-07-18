47241336031ec15985af1a7e48732335d3f2dfca
Goodness1505 <goodnessmaemile1505@gmail.com>
2023-07-18 16:59:40 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
- * main - Print _putchar is written by ubah.
- *
- * Return: Always 0.
+ * main - print _putchar
+ * is written by abah
+ * Return: 0
  */
+
 int main(void)
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('n');
-_putchar('\n');
-
-return (0);
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
+	return (write(1, &c 1));
 }

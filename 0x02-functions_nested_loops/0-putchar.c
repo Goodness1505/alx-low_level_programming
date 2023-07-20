66f4e858babc7af7d6a094abebd9a5766e9b33e9
Goodness1505 <goodnessmaemile1505@gmail.com>
2023-07-19 18:11:53 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,18 @@
-#include "holberton.h"
-
+#include "mai.h"
 /**
- * main - Prints "Holberton" followed by a new line.
- *
+ * main - Prints putchar
  * Return: Always 0.
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
 	_putchar('o');
-	_putchar('n');
+	_putchar('h');
+	_putchar('a');
+	_putchar('e');
 	_putchar('\n');
 
 	return (0);
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,16 @@
 #include "main.h"
-
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
  */
 void print_alphabet(void)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-
-	_putchar('\n');
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+ putchar('\n');
 }

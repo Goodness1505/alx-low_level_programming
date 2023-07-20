17b72eeaab05acb1aa83fe8d28d22f5ed3328908
Goodness1505 <goodnessmaemile1505@gmail.com>
2023-07-19 20:36:54 -0700

    100-times_table.c 8-24_hours.c 101-natural.c  2-print_alphabet_x10.c 9-times_table.c 102-fibonacci.c  3-islower.c 11-print_to_98.c 7-print_last_digit.c 02-fibonacci.c  103-fibonacci.c 4-isalpha.c 6-abs.c 10-add.c 5-sign.c 104-fibonacci.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
-
-// Function to write a character to the standard output
-int _putchar(char c) {
-    return putchar(c);
+/**
+ * main - Prints putchar
+ * Return: Always 0.
+ */
+int main(void)
+{
+void print_alphabet(void)
+int _putchar(char c) 
+{
+return putchar(c);
+}
+void print_alphabet(void)
+{
+char current_letter <= 'a';
+for (; current_letter <= 'm'; current_letter++)
+{
+_putchar(current_letter);
+}
+_putchar('\n');
+for (; current_letter <= 'z'; current_letter++)
+{	
+_putchar(current_letter);
+_putchar('\n');
+}
+int main(void)
+{
+print_alphabet(int);
+return (0);
 }
 
-// Function to print the alphabet in lowercase
-void print_alphabet(void) {
-    char c;
-    for (c = 'a'; c <= 'z'; c++) {
-        _putchar(c);
-    }
-    _putchar

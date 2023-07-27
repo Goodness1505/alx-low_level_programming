364d9d4c35f4eaa7b4f3eb75d4b76f2dabd25ed3
Goodness1505 <goodnessmaemile1505@gmail.com>
2023-07-27 01:44:05 -0700

    updated

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,13 +17,13 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 	j = 0;
-	while(rc[j] !='\0')
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j]
 			i++
 			j++
 	}
 
-	dest[i]='\0';
+	dest[i] = '\0';
 	return (dest);
 }

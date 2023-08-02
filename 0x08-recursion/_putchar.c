929a65e1276844bc79d5c604b0b2024710c199dc
Goodness1505 <goodnessmaemile1505@gmail.com>
2023-08-02 03:16:42 -0700

    _putchar.c

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,12 @@
+
+Write the function that prints a string, followed by a new line.
+
+phototype: void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+endif 
